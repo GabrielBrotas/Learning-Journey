f7af27372c4572516cd9adf9cf6215bf5df13fd6
Gabriel Brotas <gabriel.brotas@wexinc.com>
2023-10-03 16:17:41 -0300

    auth0

---
+++ examples/auth0/01-login/platform/router/router.go
@@ -0,0 +1,40 @@
+package router
+
+import (
+	"encoding/gob"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+
+	"01-Login/platform/authenticator"
+	"01-Login/platform/middleware"
+	"01-Login/web/app/callback"
+	"01-Login/web/app/home"
+	"01-Login/web/app/login"
+	"01-Login/web/app/logout"
+	"01-Login/web/app/user"
+)
+
+// New registers the routes and returns the router.
+func New(auth *authenticator.Authenticator) *gin.Engine {
+	router := gin.Default()
+
+	// To store custom types in our cookies,
+	// we must first register them using gob.Register
+	gob.Register(map[string]interface{}{})
+
+	store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("auth-session", store))
+
+	router.Static("/public", "web/static")
+	router.LoadHTMLGlob("web/template/*")
+
+	router.GET("/", home.Handler)
+	router.GET("/login", login.Handler(auth))
+	router.GET("/callback", callback.Handler(auth))
+	router.GET("/user", middleware.IsAuthenticated, user.Handler)
+	router.GET("/logout", logout.Handler)
+
+	return router
+}
---
+++ examples/auth0/02-istio-and-auth0/README.md
@@ -0,0 +1,3 @@
+# Examples
+
+- [securing-kubernetes-clusters-with-istio-and-auth0](https://auth0.com/blog/securing-kubernetes-clusters-with-istio-and-auth0/)
